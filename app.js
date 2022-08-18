e98430841125ad94c23149c255751d205a0522a4
nicobrevis <80666522+nicobrevis@users.noreply.github.com>
2022-08-18 00:29:06 -0400

    logs

--- app.js
+++ app.js
@@ -135,7 +135,8 @@ const listenMessage = () => client.on('message', async msg => {
 
 client = new Client({
         authStrategy: new LocalAuth(),
-        puppeteer: { headless: true 
+        puppeteer: { headless: true , 
+            args: ["--no-sandbox"]
         }
     });
     
